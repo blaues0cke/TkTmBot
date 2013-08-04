3902a062d975a95ed2426f7f2c936a71e073127a
blaues0cke <thomas@thomaskekeisen.de>
2013-08-04 14:10:25 +0200

    Initial commit.

---
+++ TkTmBot/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace TkTmBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(10, 10);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(690, 457);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.checkBox2);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(682, 431);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Aufnahme";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(443, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(553, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Abspiel-Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(427, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Durch drücken der \"Backspace\"-Taste wird eine neue Aufnahme gestartet.";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(430, 29);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(129, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Screenshot speichern";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(430, 6);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Aufnahme ist aktiv";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(421, 425);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Verzögerung";
+            this.columnHeader1.Width = 85;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Typ";
+            this.columnHeader2.Width = 62;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Taste";
+            this.columnHeader3.Width = 166;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(682, 431);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Wiedergabe";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(430, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(530, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 477);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "TkTmBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ TkTmBot/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using gma.System.Windows;
+
+namespace TkTmBot
+{
+    public partial class Form1 : Form
+    {
+
+
+        public bool UpKeyDown = false;
+        public bool DownKeyDown = false;
+        public bool LeftKeyDown = false;
+        public bool RightKeyDown = false;
+        public bool SpaceDown = false;
+
+
+
+        public long LastKeyLog;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+        public void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+
+
+                if ((e.KeyData == Keys.Up && !UpKeyDown) || (e.KeyData == Keys.Down && !DownKeyDown) || (e.KeyData == Keys.Left && !LeftKeyDown) || (e.KeyData == Keys.Right && !RightKeyDown) || (e.KeyData == Keys.Space && !SpaceDown))
+                {
+                    listView1.Items.Add(new ListViewItem(new string[] { (GetTickCount() - LastKeyLog).ToString() + " ms", "Gedrückt", e.KeyData.ToString() }));
+                    LastKeyLog = GetTickCount();
+                }
+                if (e.KeyData == Keys.Up && !UpKeyDown) {
+                    UpKeyDown = true;
+                }
+                if (e.KeyData == Keys.Down && !DownKeyDown)
+                {
+                    DownKeyDown = true;
+                }
+                if (e.KeyData == Keys.Left && !LeftKeyDown) 
+                {
+                    LeftKeyDown = true;
+                } 
+                if (e.KeyData == Keys.Right && !RightKeyDown)
+                {
+                    RightKeyDown = true;
+                }
+                if (e.KeyData == Keys.Space && !SpaceDown)
+                {
+                    SpaceDown = true;
+                }
+                
+
+
+            }
+        }
+
+        public void MyKeyUp(object sender, KeyEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+
+                if (e.KeyData == Keys.Back)
+                {
+                    listView1.Items.Clear();
+                    listView1.Items.Add(new ListViewItem(new string[] { "0 ms", "Losgelassen", e.KeyData.ToString() }));
+
+
+                }
+                else if (e.KeyData == Keys.Up || e.KeyData == Keys.Down || e.KeyData == Keys.Left || e.KeyData == Keys.Right || e.KeyData == Keys.Space)
+                {
+
+                    listView1.Items.Add(new ListViewItem(new string[] { (GetTickCount() - LastKeyLog).ToString() + " ms", "Losgelassen", e.KeyData.ToString() }));
+
+                    if (e.KeyData == Keys.Up)
+                    {
+                        UpKeyDown = false;
+                    }
+                    if (e.KeyData == Keys.Down)
+                    {
+                        DownKeyDown = false;
+                    }
+                    if (e.KeyData == Keys.Left)
+                    {
+                        LeftKeyDown = false;
+                    }
+                    if (e.KeyData == Keys.Right)
+                    {
+                        RightKeyDown = false;
+                    }
+                    if (e.KeyData == Keys.Space)
+                    {
+                        SpaceDown = false;
+                    }
+
+                }
+                LastKeyLog = GetTickCount();
+
+
+            }
+        }
+
+
+
+
+
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern long GetTickCount();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LastKeyLog = GetTickCount();
+        }
+
+        UserActivityHook actHook;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actHook = new UserActivityHook(); // crate an instance with global hooks
+
+            actHook.KeyDown += new KeyEventHandler(MyKeyDown);
+            actHook.KeyUp += new KeyEventHandler(MyKeyUp);
+        }
+
+
+
+        
+        [DllImport("user32.dll")]
+        private static extern int keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        public enum KeyboardEventFlags
+        {
+            KEYDOWN = 0,
+            KEYUP = 2
+        } 
+        
+      private void SendKeystroke(Keys key) {
+         keybd_event((byte)key, 0, 0, 0);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           // textBox1.Focus();
+            Thread test = new Thread(new ThreadStart(tttt));
+            test.Start();
+        }
+
+
+        public void tttt()
+        {
+            
+            Thread.Sleep(5000);
+           // keybd_event(Keys.Up, 0, KeyboardEventFlags.KEYDOWN, UIntPtr.Zero);
+            Thread.Sleep(2000);
+            //keybd_event(Keys.Up, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+            
+             /*
+            //SendKeys.SendWait("{UP}");
+            SendKeystroke(Keys.Up);*/
+            Thread.Sleep(100);
+            tttt();
+        }
+
+        int pos = 0;
+
+
+        public void ttt()
+        {
+
+            Thread.Sleep(Convert.ToInt16(listView1.Items[pos].SubItems[0].Text.ToString().Replace(" ms", "")));
+            
+            /*
+            if (listView1.Items[pos].SubItems[1].Text.ToString() == "Losgelassen")
+            {
+               
+                
+                switch (listView1.Items[pos].SubItems[2].Text.ToString())
+                {
+                    case "Back":
+                        //keybd_event(Keys.Back, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                        SendKeys.SendWait("{DOWN}");
+                        break;
+                    case "Up":
+                        SendKeys.SendWait("{UP}");
+                       // keybd_event(Keys.Up, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                        break;
+                    case "Left":
+                        //keybd_event(Keys.Left, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                        SendKeys.SendWait("{LEFT}");
+                        break;
+                    case "Right":
+                        //keybd_event(Keys.Right, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                        SendKeys.SendWait("{RIGHT}");
+                        break;
+
+                    
+                case "Return":
+                    SendKeys.SendWait("{ENTER}");
+                    break;
+
+
+                default:
+                    SendKeys.SendWait(listView1.Items[pos].SubItems[2].Text.ToString());
+                    break;
+
+                }
+
+            }
+            else if (listView1.Items[pos].SubItems[1].Text.ToString() == "Gedrückt")
+            {
+              
+                switch (listView1.Items[pos].SubItems[2].Text.ToString())
+                {
+                    case "Back":
+                        keybd_event(Keys.Back, 0, KeyboardEventFlags.KEYDOWN, UIntPtr.Zero);
+                        break;
+                    case "Up":
+                        keybd_event(Keys.Up, 0, KeyboardEventFlags.KEYDOWN, UIntPtr.Zero);
+                        break;
+                    case "Left":
+                        keybd_event(Keys.Left, 0, KeyboardEventFlags.KEYDOWN, UIntPtr.Zero);
+                        break;
+                    case "Right":
+                        keybd_event(Keys.Right, 0, KeyboardEventFlags.KEYDOWN, UIntPtr.Zero);
+                        break;
+
+                        /*
+                    case "Return":
+                        SendKeys.SendWait("{ENTER}");
+                        break;
+
+
+                    default:
+                        SendKeys.SendWait(listView1.Items[pos].SubItems[2].Text.ToString());
+                        break;
+
+                }
+
+            }
+*/
+            switch (listView1.Items[pos].SubItems[2].Text.ToString())
+            {
+                case "Back":
+                    //keybd_event(Keys.Back, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                    SendKeys.SendWait("{DOWN}");
+                    break;
+                case "Up":
+                    SendKeys.SendWait("{UP}");
+                    // keybd_event(Keys.Up, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                    break;
+                case "Left":
+                    //keybd_event(Keys.Left, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                    SendKeys.SendWait("{LEFT}");
+                    break;
+                case "Right":
+                    //keybd_event(Keys.Right, 0, KeyboardEventFlags.KEYUP, UIntPtr.Zero);
+                    SendKeys.SendWait("{RIGHT}");
+                    break;
+            }
+
+            
+                pos++;
+            if (pos < listView1.Items.Count)
+            {
+                ttt();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(listView1.Items[pos].SubItems[Convert.ToInt16(textBox1.Text)].Text.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            keybd_event((byte)Keys.Up, 0, 0, 0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            keybd_event((byte)Keys.Up, 0, 2, 0);
+        }
+
+
+    }
+}
